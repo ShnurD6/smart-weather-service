48e1044c2d5cbb43e540d603dea9806c4b7d1b6a
ShnurD6 <shnurd6@mail.ru>
2020-05-12 01:14:29 +0300

    Switch boost::beast to CprLib (CurlForPeople)

--- Dockerfile.dockerfile
+++ Dockerfile.dockerfile
@@ -6,11 +6,18 @@ RUN apt-get update && \
         libboost-system-dev \
         libboost-date-time-dev \
         cmake && \
+#   Install TgBot
     git clone https://github.com/reo7sp/tgbot-cpp && \
     cd tgbot-cpp && \
     cmake . -DCMAKE_CXX_COMPILER=/usr/bin/clang++-11 && \
     make -j4 && \
-    make install
+    make install && \
+    cd .. && \
+#   Install CurlForPeople lib
+    git clone --recursive https://github.com/whoshuu/cpr.git && \
+    cd cpr && \
+    cmake . -DBUILD_CPR_TESTS=OFF -DCMAKE_CXX_COMPILER=/usr/bin/clang++-11 && \
+    make install -j4
 
 ADD Sources /app/Sources
 ADD CMakeLists.txt *.api /app/
--- Sources/ApiConnection/ApiConnection.hpp
+++ Sources/ApiConnection/ApiConnection.hpp
@@ -1,81 +1,35 @@
 #pragma once
 
 #include <iostream>
-#include <boost/beast.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/asio.hpp>
 #include "JsonParser/JsonParser.hpp"
 #include "Logger/Logger.hpp"
+#include <cpr/cpr.h>
 
 using namespace std::string_literals;
-namespace http = boost::beast::http;
-
 class ApiConnection
 {
 public:
 
     ApiConnection(
-        const char* aHost,
-        const char* aApiTarget)
-        : mResolver(mIoc)
-        , mSocket(mIoc)
-        , mHost(aHost)
-        , mApiTarget(aApiTarget)
-    {
-        Log("Connecting to "s + mHost + "..."s, Logger::LogType::Debug);
-        boost::asio::connect(mSocket, mResolver.resolve(mHost, "80"));
-        Log("Connecting to "s + mHost + " success!"s, Logger::LogType::Debug);
-    };
-
-    ~ApiConnection()
-    {
-        mSocket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-    }
+        const std::string& aHost,
+        const std::string& aApiTarget)
+        : mRequestUrl(aHost + aApiTarget)
+    {};
 
     [[nodiscard]] std::string MakeRequest(const std::string& aQueryString) const
     {
-        http::request<http::string_body> request(http::verb::get, mApiTarget + aQueryString, 11);
-        request.set(http::field::host, mHost);
-        request.prepare_payload();
+        std::string urlWithQuery{mRequestUrl + aQueryString};
 
-        Log("Send " + aQueryString + " to " + mHost + "...", Logger::LogType::Debug);
+        Log("Send " + aQueryString + " to " + mRequestUrl + "...", Logger::LogType::Debug);
 
-        try
-        {
-            http::write(mSocket, request);
-        }
-        catch (std::exception& aException)
-        {
-            Log("Catch exception when write socket: "s + aException.what(), Logger::LogType::Error);
-        }
+        auto r = cpr::Get(cpr::Url{urlWithQuery});
 
-        boost::beast::flat_buffer buffer;
-
-        http::response<http::string_body> result;
-
-        try
-        {
-            http::read(mSocket, buffer, result);
-        }
-        catch (std::exception& aException)
-        {
-            Log("Catch exception when read from socket: "s + aException.what(), Logger::LogType::Error);
-        }
-
-        Log("Get result from "s + mHost, Logger::LogType::Debug);
-
-        return result.body();
+        return r.text;
     }
 
 private:
 
-    boost::asio::io_context mIoc;
-
-    boost::asio::ip::tcp::resolver mResolver;
-    mutable boost::asio::ip::tcp::socket mSocket;
-
-    const char* mHost;
-    const char* mApiTarget;
+    std::string mRequestUrl;
 
     template <typename T>
     void Log(T&& aNewLog, Logger::LogType aLogType) const
